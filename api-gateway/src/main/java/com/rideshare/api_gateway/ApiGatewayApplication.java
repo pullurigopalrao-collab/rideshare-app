4f781f378d8cd167894d7ddaef6323f746f48a09
Gopal Rao Pulluri <pulluri.gopalrao@gmail.com>
2025-11-05 11:26:40 +0530

    Integrated the logs with Splunk with Splunk OTEL agent

--- api-gateway/src/main/java/com/rideshare/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/rideshare/api_gateway/ApiGatewayApplication.java
@@ -1,12 +1,9 @@
 package com.rideshare.api_gateway;
 
-import io.micrometer.tracing.Tracer;
-import org.slf4j.MDC;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.server.WebFilter;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -16,20 +13,9 @@ public class ApiGatewayApplication {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
-    @Bean
-    public WebFilter mdcTraceFilter(Tracer tracer) {
-        return (exchange, chain) -> chain.filter(exchange)
-                .contextWrite(ctx -> {
-                    var span = tracer.currentSpan();
-                    if (span != null) {
-                        MDC.put("traceId", span.context().traceId());
-                        MDC.put("spanId", span.context().spanId());
-                    }
-                    return ctx;
-                })
-                .doFinally(signalType -> {
-                    MDC.remove("traceId");
-                    MDC.remove("spanId");
-                });
+    @PostConstruct
+    public void enableReactorContext() {
+        reactor.core.publisher.Hooks.enableAutomaticContextPropagation();
     }
+
 }
--- user-service/src/main/java/com/rideshare/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/rideshare/userservice/UserServiceApplication.java
@@ -1,16 +1,16 @@
 package com.rideshare.userservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-
+@Slf4j
 @EnableCaching
 @SpringBootApplication
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
-
         SpringApplication.run(UserServiceApplication.class, args);
-	}
+    }
 
 }
